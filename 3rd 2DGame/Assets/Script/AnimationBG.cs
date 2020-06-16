bda633b5cd41391feb8961aff980b2e4bef92aa7
SanhOwO <281870525@qq.com>
2020-06-16 10:56:19 +0800

    3rd 2DGame
    2020 6 16

---
+++ 3rd 2DGame/Assets/Script/AnimationBG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBG : MonoBehaviour
+{
+    Material ml;
+
+    Vector2 movement;
+
+    public Vector2 speed = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ml = GetComponent<Renderer>().material ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement += speed;
+        ml.mainTextureOffset = movement;
+    }
+}
