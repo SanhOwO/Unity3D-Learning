59823cc8fe254b38b559373f1688721b42694aa2
SanhOwO <281870525@qq.com>
2020-06-19 18:22:04 +0800

    异度加载AsyncOperation
    使用携程控制

---
+++ 2D-Obejct pool/Assets/Scripts/LoadManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadManager : MonoBehaviour
+{
+    public GameObject loadScreen;
+    public Slider slider;
+    public Text text;
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        loadScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex+1);
+        operation.allowSceneActivation = false;//暂时不允许加载新场景
+
+        while (!operation.isDone)
+        {
+            slider.value = operation.progress;//0 -- 1
+
+            text.text = operation.progress * 100 + "%";
+
+            if(operation.progress >= 0.9f) //设定完成加载会卡在0.9
+            {
+                slider.value = 1;
+                text.text = "Press Any key to continue";
+                if (Input.anyKeyDown)
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+}
